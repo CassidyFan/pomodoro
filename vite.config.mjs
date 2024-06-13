6726a0b00f67c52bb9cd63f77a4f8e84493d1c44
Cassidy <cassidyfan0206@gmail.com>
2024-06-13 11:43:58 +0800

    first commit

---
+++ vite.config.mjs
@@ -0,0 +1,106 @@
+// Plugins
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import Fonts from 'unplugin-fonts/vite'
+import Layouts from 'vite-plugin-vue-layouts'
+import Vue from '@vitejs/plugin-vue'
+import VueRouter from 'unplugin-vue-router/vite'
+import Vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import { VitePluginRadar } from 'vite-plugin-radar'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// Utilities
+import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from 'node:url'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        cleanupOutdatedCaches: true,
+        globPatterns: [
+          '**/*.{html,css,js,jpg,png,svg,gif,woff2,ico,ttf}**'
+        ],
+        ignoreURLParametersMatching: [/.*/]
+      },
+      manifest: {
+        name: '番茄鐘',
+        short_name: '番茄鐘',
+        theme_color: '#ffa7a7',
+        background_color: '#ffa7a7',
+        start_url: './',
+        scope: './',
+        display: 'standalone',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    }),
+    VitePluginRadar({
+      analytics: {
+        id: 'G-P5DB1RJJ06'
+      }
+    }),
+    VueRouter(),
+    Layouts(),
+    Vue({
+      template: { transformAssetUrls }
+    }),
+    // https://github.com/vuetifyjs/vuetify-loader/tree/master/packages/vite-plugin#readme
+    Vuetify({
+      autoImport: true,
+      styles: {
+        configFile: 'src/styles/settings.scss'
+      }
+    }),
+    Components(),
+    Fonts({
+      google: {
+        families: [{
+          name: 'Roboto',
+          styles: 'wght@100;300;400;500;700;900'
+        }]
+      }
+    }),
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router'
+      ],
+      eslintrc: {
+        enabled: true
+      },
+      vueTemplate: true
+    })
+  ],
+  define: { 'process.env': {} },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.vue'
+    ]
+  },
+  server: {
+    port: 3000
+  }
+})
